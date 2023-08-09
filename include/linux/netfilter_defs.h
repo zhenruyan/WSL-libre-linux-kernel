e80a54af8316666a560062b4c9f1e06b340ba083
Freedo <freedo@linux-libre.fsfla.org>
2023-08-09 00:19:18 +0000

    GNU Linux-libre 4.14.321-gnu1

---
+++ include/linux/netfilter_defs.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __LINUX_NETFILTER_CORE_H_
+#define __LINUX_NETFILTER_CORE_H_
+
+#include <uapi/linux/netfilter.h>
+
+#define NF_MAX_HOOKS	NF_INET_NUMHOOKS
+
+#endif
