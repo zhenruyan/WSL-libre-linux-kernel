0d39ce1bba856816930297944cd6f0689783f0a1
Freedo <freedo@linux-libre.fsfla.org>
2024-04-28 23:28:28 +0000

    GNU Linux-libre 6.9-rc6-gnu

---
+++ drivers/gpu/drm/xe/compat-i915-headers/intel_uc_fw.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: MIT */
+/*
+ * Copyright © 2023 Intel Corporation
+ */
+
+#ifndef _INTEL_UC_FW_H_
+#define _INTEL_UC_FW_H_
+
+#define INTEL_UC_FIRMWARE_URL "/*(DEBLOBBED)*/"
+
+#endif
