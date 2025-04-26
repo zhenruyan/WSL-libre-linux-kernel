ed848434de1f4f3d8ff47d348edcb3ea99c68cd0
Freedo <freedo@linux-libre.fsfla.org>
2025-04-26 09:39:06 +0000

    GNU Linux-libre 6.14.4-gnu

---
+++ drivers/gpu/drm/i915/display/intel_plane_initial.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: MIT */
+/*
+ * Copyright © 2021 Intel Corporation
+ */
+
+#ifndef __INTEL_PLANE_INITIAL_H__
+#define __INTEL_PLANE_INITIAL_H__
+
+struct intel_display;
+
+void intel_initial_plane_config(struct intel_display *display);
+
+#endif
