ec29c66df21040d0193a5cb16bc1c2164137fd86
Freedo <freedo@linux-libre.fsfla.org>
2026-09-02 18:52:38 +0000

    GNU Linux-libre 6.18.49-gnu

---
+++ drivers/net/wireless/st/cw1200/fwio.h
@@ -0,0 +1,36 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Firmware API for mac80211 ST-Ericsson CW1200 drivers
+ *
+ * Copyright (c) 2010, ST-Ericsson
+ * Author: Dmitry Tarnyagin <dmitry.tarnyagin@lockless.no>
+ *
+ * Based on:
+ * ST-Ericsson UMAC CW1200 driver which is
+ * Copyright (c) 2010, ST-Ericsson
+ * Author: Ajitpal Singh <ajitpal.singh@stericsson.com>
+ */
+
+#ifndef FWIO_H_INCLUDED
+#define FWIO_H_INCLUDED
+
+#define BOOTLOADER_CW1X60       "/*(DEBLOBBED)*/"
+#define FIRMWARE_CW1X60		"/*(DEBLOBBED)*/"
+#define FIRMWARE_CUT22		"/*(DEBLOBBED)*/"
+#define FIRMWARE_CUT20		"/*(DEBLOBBED)*/"
+#define FIRMWARE_CUT11		"/*(DEBLOBBED)*/"
+#define FIRMWARE_CUT10		"/*(DEBLOBBED)*/"
+#define SDD_FILE_CW1X60		"/*(DEBLOBBED)*/"
+#define SDD_FILE_22		"/*(DEBLOBBED)*/"
+#define SDD_FILE_20		"/*(DEBLOBBED)*/"
+#define SDD_FILE_11		"/*(DEBLOBBED)*/"
+#define SDD_FILE_10		"/*(DEBLOBBED)*/"
+
+int cw1200_load_firmware(struct cw1200_common *priv);
+
+/* SDD definitions */
+#define SDD_PTA_CFG_ELT_ID 0xEB
+#define SDD_REFERENCE_FREQUENCY_ELT_ID 0xc5
+u32 cw1200_dpll_from_clk(u16 clk);
+
+#endif
